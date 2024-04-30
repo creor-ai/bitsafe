ed829e311c21f131273ce0ee8e54d982a60c1fe0
omnicreor <fatihturker35@gmail.com>
2024-04-30 22:01:38 +0300

    Remove unused dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
   runApp(App());
 }
